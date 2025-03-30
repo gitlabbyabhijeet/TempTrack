cb4b6343f052e49795031e1f678a1de206d6e40f
gitlabbyabhijeet <bhartiabhijeet67@gmail.com>
2025-03-30 11:15:17 +0530

    Updated package.json and vite.config.js for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/TempTrack/", // Make sure this matches your GitHub repo name
-});
-
+  base: "/TempTrack/",
+})
